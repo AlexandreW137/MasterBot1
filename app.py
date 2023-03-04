111a030032e7a0d5c2e573a3c49498dff4d2756e
AlexandreXD <87343139+AlexandreW137@users.noreply.github.com>
2023-03-04 00:52:11 -0300

    home

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ app = Flask(__name__, template_folder="../templates")
 
 @app.route('/')
 def index():
-    twitch_auth_url = f"https://id.twitch.tv/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URL}&response_type=code&scope=user_subscriptions"
-    return f"<a href='{twitch_auth_url}'>Login com Twitch</a>"
+    return f"<a href=''>Login com Twitch</a>"
 
 
 @app.route('/sorteio')
 def sorteio():
-    return render_template('sorteio.html')
+    return render_template('arquivos/app/templates/sorteio.html')
 if __name__ == '__main__':
     app.run()
