d5362f45b23b0bfecbe64f2ad345e28dbbc864d8
AlexandreXD <87343139+AlexandreW137@users.noreply.github.com>
2023-03-04 02:46:58 -0300

    subs

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, render_template
 from requests import request
 import requests
 from decouple import config
+import json
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 SECRET_KEY = config('SECRET_KEY')   
@@ -37,7 +38,15 @@ def callback():
 }
     response = requests.post(TOKEN_URL, params=params)
     access_token = response.json().get('acess_token')
-    print(access_token)
+    
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Client-Id': CLIENT_ID
+    }
+    response = requests.get(f'{API_URL}/subscriptions', headers=headers)
+    subscribers = [subscription['user_name'] for subscription in response.json()['data']]
+
+    return render_template('subscribers.html', subscribers=subscribers)
 
 
 if __name__ == '__main__':
